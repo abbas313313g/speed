add9c827fbb37469077e24526e8bf67dc025a837
abbas 313 <prsmstr4@gmail.com>
2026-07-16 05:20:11 +0000

    Try fixing this error: `Runtime Error: Error: cn is not defined. Error s

--- src/components/DeliveryMap.tsx
+++ src/components/DeliveryMap.tsx
@@ -3,6 +3,7 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import { Loader2, Bike, Store, Home } from 'lucide-react';
+import { cn } from "@/lib/utils";
 
 interface DeliveryMapProps {
   origin: { lat: number, lng: number };
